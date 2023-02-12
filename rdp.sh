ba08b53f1f3813ad1cd307ab292962945a9a2741
cloudmar <124941397+cloudmar@users.noreply.github.com>
2023-02-12 10:35:00 +0100

    Update and rename rdp to rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,4 +1,5 @@
-# INstall remote desktop connection
+# install remote desktop connection
+# don't forget to use ssh_password.sh if you want to login using a password
 
 sudo apt update
 sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils
